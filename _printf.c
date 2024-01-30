3a24e66271545af43c6fdb69ff7dd3455a76e039
Marouane <mar.maatouk@gmail.com>
2024-01-30 14:18:35 +0100

    commit

--- _printf.c
+++ _printf.c
@@ -7,89 +7,88 @@
  */
 int _printf(const char *format, ...)
 {
-    if (format == NULL)
-        return (-1);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    va_list args;
-    va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-    int printed_chars = 0;
-    int buff_ind = 0;
-    char buffer[BUFF_SIZE];
+	va_start(list, format);
 
-    while (*format != '\0')
-    {
-        if (*format != '%')
-        {
-            buffer[buff_ind++] = *format;
-            if (buff_ind == BUFF_SIZE - 1)
-            {
-                print_buffer(buffer, &buff_ind);
-                printed_chars += buff_ind;
-            }
-            else
-            {
-                printed_chars++;
-            }
-        }
-        else
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    buffer[buff_ind++] = va_arg(args, int);
-                    break;
-                case 's':
-                    printed_chars += print_string(args, buffer, &buff_ind);
-                    break;
-                case '%':
-                    buffer[buff_ind++] = '%';
-                    break;
-                default:
-                    buffer[buff_ind++] = '%';
-                    buffer[buff_ind++] = *format;
-            }
-        }
-        format++;
-    }
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+			{
+				print_buffer(buffer, &buff_ind);
+				printed_chars += buff_ind;
+			}
+			else
+			{
+				printed_chars++;
+			}
+		}
+		else
+		{
+			format++;
+			switch (format[i])
+			{
+				case 'c':
+					buffer[buff_ind++] = va_arg(list, int);
+					break;
+				case 's':
+					printed_chars += print_string(list, buffer, &buff_ind);
+					break;
+				case '%':
+					buffer[buff_ind++] = '%';
+					break;
+				default:
+					buffer[buff_ind++] = '%';
+					buffer[buff_ind++] = format[i];
+			}
+		}
+	}
 
-    print_buffer(buffer, &buff_ind);
-    va_end(args);
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
 
-    return (printed_chars);
+	return (printed_chars);
 }
 
 /**
  * print_string - Prints a string
- * @args: va_list for variable arguments
+ * @list: va_list for variable arguments
  * @buffer: Character buffer
  * @buff_ind: Index at which to add the next char
  * Return: Number of characters printed
  */
-int print_string(va_list args, char buffer[], int *buff_ind)
+int print_string(va_list list, char buffer[], int *buff_ind)
 {
-    char *s = va_arg(args, char *);
-    if (s == NULL)
-        s = "(null)";
+	char *s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(null)";
 
-    int len = 0;
-    while (*s)
-    {
-        buffer[*buff_ind] = *s;
-        (*buff_ind)++;
-        if (*buff_ind == BUFF_SIZE - 1)
-        {
-            print_buffer(buffer, buff_ind);
-            len += *buff_ind;
-        }
-        else
-        {
-            len++;
-        }
-        s++;
-    }
-    return len;
+	int len = 0;
+	while (*s)
+	{
+		buffer[*buff_ind] = *s;
+		(*buff_ind)++;
+		if (*buff_ind == BUFF_SIZE - 1)
+		{
+			print_buffer(buffer, buff_ind);
+			len += *buff_ind;
+		}
+		else
+		{
+			len++;
+		}
+		s++;
+	}
+	return len;
 }
 
 /**
@@ -99,9 +98,9 @@ int print_string(va_list args, char buffer[], int *buff_ind)
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
-    if (*buff_ind > 0)
-    {
-        write(1, buffer, *buff_ind);
-        *buff_ind = 0;
-    }
+	if (*buff_ind > 0)
+	{
+		write(1, buffer, *buff_ind);
+		*buff_ind = 0;
+	}
 }
