a669ed8298864820dd8edec5952a0e88d6bdc757
Marouane <mar.maatouk@gmail.com>
2024-01-30 13:52:56 +0100

    commit

---
+++ printf.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ * @...: Variable number of arguments
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+    const char *ptr;
+    char c;
+
+    va_start(args, format);
+
+    for (ptr = format; *ptr != '\0'; ptr++)
+    {
+        if (*ptr != '%')
+        {
+            write(1, ptr, 1);
+            count++;
+        }
+        else
+        {
+            ptr++;
+            switch (*ptr)
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    count++;
+                    break;
+                case 's':
+                    count += _print_str(va_arg(args, char *));
+                    break;
+                case '%':
+                    write(1, "%", 1);
+                    count++;
+                    break;
+                default:
+                    write(1, "%", 1);
+                    write(1, ptr, 1);
+                    count += 2;
+            }
+        }
+    }
+
+    va_end(args);
+
+    return count;
+}
+
+/**
+ * _print_str - Print a string
+ * @str: String to print
+ *
+ * Return: Number of characters printed
+ */
+int _print_str(char *str)
+{
+    int count = 0;
+
+    if (str == NULL)
+        str = "(null)";
+
+    while (*str != '\0')
+    {
+        write(1, str, 1);
+        str++;
+        count++;
+    }
+
+    return count;
+}
